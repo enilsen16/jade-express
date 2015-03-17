d6d824b468a9eb94367d38a21f3b0b533679bf09
Erik Nilsen <enilsen16@live.com>
2015-03-16 17:11:56 -0700

    add nodetest1

---
+++ nodetest1/routes/index.js
@@ -0,0 +1,46 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+
+router.get('/helloworld', function(req, res) {
+  res.render('helloworld', { title: 'Hello, World!' });
+});
+
+router.get('/userlist', function(req, res) {
+  var db = req.db;
+  var collection = db.get('usercollection');
+  collection.find({},{}, function(error, docs) {
+    res.render('userlist', {
+      "userlist" : docs
+    });
+  });
+});
+
+router.get('/newuser', function(req, res) {
+  res.render('newuser', { title: 'Add New User '});
+});
+
+router.post('/adduser', function(req, res) {
+  var db = req.db;
+  var userName = req.body.username;
+  var userEmail = req.body.useremail;
+  var collection = db.get('usercollection');
+
+  collection.insert({
+    "username" : userName,
+    "email" : userEmail
+  }, function (err, doc) {
+    if (err) {
+      res.send('There was a problem adding this information to the database');
+    } else {
+      res.location('userlist');
+      res.redirect('userlist');
+    }
+  });
+});
+
+module.exports = router;
